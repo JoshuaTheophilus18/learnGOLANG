36a2d3f450ff9b62aa15e064aeb15fab60a8ef5b
JoshuaTheophilus18 <joshua.fortidigitalstudio@gmail.com>
2021-03-16 15:39:50 +0700

    Add Channel usage on learnRoutine

--- learnRoutine.go
+++ learnRoutine.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func print(till int, message string) {
@@ -14,6 +15,32 @@ func LearnGoRoutine() {
 	go print(5, "halo")   //pemanggilan fungsi dengan goroutine
 	print(5, "apa kabar") //pemanggilan fungsi biasa
 
-	var input string
-	fmt.Scanln(&input)
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	var messages = make(chan string) //pembuatan channel (komunikasi antar goroutine)
+
+	var sayHelloTo = func(who string) {
+		var data = fmt.Sprintf("hello %s", who)
+		messages <- data
+	}
+
+	go sayHelloTo("john wick")
+	go sayHelloTo("ethan hunt")
+	go sayHelloTo("jason bourne")
+
+	var message1 = <-messages
+	fmt.Println(message1)
+
+	var message2 = <-messages
+	fmt.Println(message2)
+
+	var message3 = <-messages
+	fmt.Println(message3)
+
+	// var input string
+	// fmt.Scanln(&input)
+	time.Sleep(time.Second)
+	fmt.Println("done")
 }
